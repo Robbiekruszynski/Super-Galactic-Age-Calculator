99f27b3f46b621edd4fd7bbcd162c46e448fd72e
Student <example@example.com>
2019-03-15 16:01:32 -0700

    clean up code and check logic

--- spec/tester-spec.js
+++ spec/tester-spec.js
@@ -1,17 +1,12 @@
-import { Old } from './../src/aliveTimer.js';
-import { AgeCalc  } from './../src/planetAge.js';
+import { Old  } from './../src/planetAge.js';
+import { AgeCalc  } from'./../src/aliveTimer.js';
 
 
-describe('AgeCalc', function(){
+  describe('AgeCalc', function(){
 
-  it('should return the correct age for the user on Mercury after they complete the form', function() {
-  const age = new AgeCalc(29);
-  expect(age.aliveOnMercury(29)).toEqual(121);
-});
-
-  it('should return the correct age for the user on Mercury after they complete the form', function() {
-  expect()
+  it('should return the correct age for the user on earth after they complete the form', function() {
+    let birthday = new AgeCalc("914,544,000");
+    expect(birthday.findAge()).toEqual(29);
   });
-
 
 });
