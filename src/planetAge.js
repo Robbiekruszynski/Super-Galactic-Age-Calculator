a453a57af85b54525cf4824b431ea1ec44bc3a21
Student <example@example.com>
2019-03-15 11:49:29 -0700

    rework on planetAge.js

--- src/planetAge.js
+++ src/planetAge.js
@@ -1,31 +1,31 @@
-export class AliveTimer {
+export class EarthTimer {
 
-    constructor(alivetimer) {
-      this.alivetimer = alivetimer;
+    constructor(earthtimer) {
+      this.earthtimer = earthtimer;
     }
 
-    inSeconds(alivetimer) {
-      let alivetimeInSeconds = alivetimer * 31536000
-      return alivetimeInSeconds;
+    inSeconds(earthtimer) {
+      let earthtimeInSeconds = earthtimer * 31536000
+      return earthtimeInSeconds;
     }
 
-    aliveOnMercury(alivetimer) {
-      let mercuryAlive = alivetimer / 0.24;
+    aliveOnMercury(earthtimer) {
+      let mercuryAlive = earthtimer / 0.24;
       return Math.round(mercuryAlive);
     }
 
-    aliveOnVenus(alivetimer) {
-      let venusAlive = alivetimer / 0.62;
+    aliveOnVenus(earthtimer) {
+      let venusAlive = earthtimer / 0.62;
       return Math.round(venusAlive);
     }
 
-    aliveOnMars(alivetimer) {
-      let marsAlive = alivetimer * 1.88;
+    aliveOnMars(earthtimer) {
+      let marsAlive = earthtimer * 1.88;
       return Math.round(marsAlive);
     }
 
-    aliveOnJupiter(alivetimer) {
-      let jupiterAlive = alivetimer * 11.86;
+    aliveOnJupiter(earthtimer) {
+      let jupiterAlive = earthtimer * 11.86;
       return Math.round(jupiterAlive);
 
     }
